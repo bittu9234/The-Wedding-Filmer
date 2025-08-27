b571baea0edbb93b9ba9dead739db5be1c90fea4
bittu9234 <kumarvivekkumar.2121@gmail.com>
2025-08-27 22:53:37 +0530

    add all file
    add all file

---
+++ script.js
@@ -0,0 +1,26 @@
+function showSection(sectionId) {
+  // all sections hide 
+  document.querySelectorAll(".content-section").forEach(sec => {
+    sec.classList.remove("active");
+  });
+
+  // only show  clicked 
+  document.getElementById(sectionId).classList.add("active");
+}
+//buttons
+const menuBtns = document.querySelectorAll('.menu-btn');
+const sections = document.querySelectorAll('.slider-section');
+// button active
+menuBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    menuBtns.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+    sections.forEach(sec => sec.classList.remove('active'));
+    document.getElementById(btn.dataset.target).classList.add('active');
+  });
+});
+
+
+document.querySelector('.view-more-btn').addEventListener('click', function() {
+  alert('View More button clicked!');
+});
